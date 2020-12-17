5b9faa263fb9da5e2e70026033370caab1eda52c
csesese <63036918+csesese@users.noreply.github.com>
2020-12-18 00:05:34 +0900

    201218 연속 계산되는데 여러번하면 계산오류 , 소수점 게산 오류

--- Calculate/Form1.cs
+++ Calculate/Form1.cs
@@ -8,97 +8,104 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+// 소숫점 문제
+
 namespace Calculate
 {
     public partial class Form1 : Form
-    { 
-        string output; //최종 결과값
+    {
+        string output; //최종 결과값p
         string cal;//사칙 연산 저장
         Double R;//사칙연산 후 결과 값
 
+        //string[] cals= { };
+        //string[] num = { };
+        List<string> cals = new List<string>();
+        List<string> num = new List<string>();
+
+
+
         public Form1()
         {
-            
+
             InitializeComponent();
         }
         //공통 메서드 : 숫자버튼 클릭하여서 texbox 에 출력
         public void printResult(string x)
         {
+
+            if (pre_Result.Text.Contains("="))
+            {
+                pre_Result.Clear();
+                text_Result.Clear();
+                num.Clear();
+                cals.Clear();
+            }
+
+
+
+            //num 배열에 값 추가
+            num.Add(x);
+            
             //text가 비어있지 않으면 추가
             if (text_Result.TextLength != 0)
             {   //초기값 일때는 제외
                 if (text_Result.Text.Equals("0"))
-                    {
-                    text_Result.Text=x;
-                    }
-                else 
-                    { 
-                    text_Result.AppendText(x); 
-                     }
-               
+                {
+                    text_Result.Text = x;
+                }
+                else
+                {
+                    text_Result.AppendText(x);
+                }
+
             }
-            else text_Result.Text = x;
+            else  text_Result.Text = x;
         }
+
 
         //공통메서드 : 사칙연산버튼을 클릭하여서 위에 pre_Result textbox 에 출력
-/*        public void printPreResult()
-        {
-            pre_Result.Text = text_Result.Text;
-            text_Result.Clear();
-
-        }*/
-
-
-        
-         //공통메서드 : 사칙연산버튼을 클릭하여서 위에 pre_Result textbox 에 출력
         public void printPreResult()
         {
-            
-            pre_Result.AppendText(text_Result.Text);
-            text_Result.Clear();
-
-            int pre_str = pre_Result.TextLength;
-            char last_pre_Result = pre_Result.Text[pre_str-1]; //마지막 문자 
-
-            //연산자는 연속사용 불가함으로, 다른 연산자를 선택하면 변경
-            if (text_Result.TextLength != 1)
+            //계산이 완료된 것에 이어서 계산하면 결과값을 기준으로 연산 다시 시작
+            if (pre_Result.Text.Contains("="))
             {
-                switch (last_pre_Result)
-                {
-                    case '+':
-                    case '-':
-                    case '*':
-                    case '/':
-                        pre_Result.Text = pre_Result.Text.Substring(0, pre_str - 1); //마지막 연산 지우기
-                        pre_Result.AppendText(text_Result.Text);
-                        
-                        break;
-                    case '=':
-                        //결과 값에 이어서 연산할때
-                        pre_Result.Clear();
-                        pre_Result.Text = text_Result.Text;
-                        break;
-                    default:
-                        pre_Result.AppendText(text_Result.Text);
-                        text_Result.Clear();
-                        break;
 
-
-                }
-                text_Result.Clear();
+                pre_Result.Clear();
+                pre_Result.Text = text_Result.Text;
             }
 
-            
-            
-            
-            
+            else
+            {
+                pre_Result.AppendText(text_Result.Text);
 
+                int pre_str = pre_Result.TextLength;
+                char last_pre_Result = pre_Result.Text[pre_str - 1]; //마지막 문자 
+
+                //연산자는 연속사용 불가함으로, 다른 연산자를 선택하면 변경
+                if (text_Result.TextLength != 1)
+                {
+                    switch (last_pre_Result)
+                    {
+                        case '+':
+                        case '-':
+                        case '*':
+                        case '/':
+                            pre_Result.Text = pre_Result.Text.Substring(0, pre_str - 1); //마지막 연산 지우기
+                            pre_Result.AppendText(text_Result.Text);
+
+                            break;
+
+                    }
+
+                }
+            }
+            text_Result.Clear(); //연산에 사용된 상수 지우기 
         }
-        
+
         //숫자 버튼 클릭 
-
-
-        private void btn_num_1_Click(object sender, EventArgs e) 
+        private void btn_num_1_Click(object sender, EventArgs e)
         {
             string num = btn_num_1.Text;
             printResult(num);
@@ -158,10 +165,6 @@ namespace Calculate
             printResult(num);
         }
 
-        
-
-
-
 
         //CLEAR 현재 입력값을 0으로 초기화
         private void btn_clear_Click(object sender, EventArgs e)
@@ -169,13 +172,13 @@ namespace Calculate
             text_Result.Clear();
             text_Result.Text = "0";
         }
-        
+
         //문자열중에 마지막 숫자 지우기 
         private void btn_del_Click(object sender, EventArgs e)
         {
             int str = text_Result.TextLength; //현재 문자열 길이
-            
-            if(str==1)
+
+            if (str == 1)
             {  //한자리 수면 0으로 초기화
                 text_Result.Text = "0";
             }
@@ -183,7 +186,7 @@ namespace Calculate
             {
                 text_Result.Text = text_Result.Text.Substring(0, str - 1);
             }
-            
+
 
         }
         //모든 것을 초기화
@@ -192,6 +195,9 @@ namespace Calculate
             text_Result.Clear();
             pre_Result.Clear();
             text_Result.Text = "0";
+
+            num.Clear();
+            cals.Clear();
         }
         //소수점 처리
         private void btn_dot_Click(object sender, EventArgs e)
@@ -199,24 +205,24 @@ namespace Calculate
             //중복 체크
             if (text_Result.Text.Contains(".") == false)
 
-            text_Result.Text += ".";
+                text_Result.Text += ".";
         }
 
         //양수 음수 처리 
         private void btn_plumin_Click(object sender, EventArgs e)
-        {   
+        {
             //문자열 정수로 변환
             int outNum = Convert.ToInt32(text_Result.Text);
-            
-            outNum = outNum * -1; 
+
+            outNum = outNum * -1;
 
             //정수를 문자열로 변환
             string intNum = Convert.ToString(outNum);
 
-            text_Result.Text = intNum ;
+            text_Result.Text = intNum;
 
 
-            
+
         }
 
         //사칙연산 처리
@@ -228,6 +234,7 @@ namespace Calculate
             pre_Result.AppendText("+");
 
             cal = "plus";
+            cals.Add(cal);
 
         }
 
@@ -238,6 +245,7 @@ namespace Calculate
             pre_Result.AppendText("-");
 
             cal = "minus";
+            cals.Add(cal);
         }
         //곱셈
         private void btn_mul_Click(object sender, EventArgs e)
@@ -246,6 +254,7 @@ namespace Calculate
             pre_Result.AppendText("*");
 
             cal = "mul";
+            cals.Add(cal);
         }
 
         //나눗셈
@@ -255,6 +264,7 @@ namespace Calculate
             pre_Result.AppendText("÷");
 
             cal = "div";
+            cals.Append(cal);
         }
 
         // - 결과값 출력
@@ -262,24 +272,40 @@ namespace Calculate
         {
 
             //사칙연산 빼고 숫자만             
-            int pre_str=pre_Result.TextLength;
-            
+            int len = pre_Result.TextLength;
+            pre_Result.AppendText(text_Result.Text);
 
-            //문자열을 Double로 변환
-            Double R1 = Convert.ToDouble(pre_Result.Text.Substring(0, pre_str - 1));
-            Double R2 = Convert.ToDouble(text_Result.Text);
 
-            //사칙연산하기 
-           
-            switch (cal)
+            //초기값 S1 지정 
+            string s1 = num[0];
+            Double R1 = Convert.ToDouble(s1);
+
+            string s2;
+            for (int i = 0; i < cals.Count; i++)
             {
-                case "plus": R = R1 + R2; break; //더하기
-                case "minus": R = R1 - R2;break;//빼기
-                case "mul": R = R1 * R2;break;//곱셈
-                case "div": R = R1 / R2;break;//나눗셈
+                s2 = num[i + 1];
+                cal = cals[i];
+
+
+                //Char을 Double로 변환    
+
+                Double R2 = Convert.ToDouble(s2);
+
+
+                //사칙연산하기 
+
+                switch (cal)
+                {
+                    case "plus": R = R1 + R2; break; //더하기
+                    case "minus": R = R1 - R2; break;//빼기
+                    case "mul": R = R1 * R2; break;//곱셈
+                    case "div": R = R1 / R2; break;//나눗셈
+
+                }
+
+                R1 = R;
 
             }
-
 
             //R 값이 정수이면 True, 실수이면 False
             if ((R % 1) == 1)
@@ -291,21 +317,15 @@ namespace Calculate
             {
                 output = R.ToString();
             }
-              
 
-
-            pre_Result.AppendText(text_Result.Text);
             pre_Result.AppendText("=");
             //결과값 출력하기 
-            text_Result.Text=output;
-
-            
+            text_Result.Text = output;
 
         }
 
-
     }
 
-    
+
 
 }
