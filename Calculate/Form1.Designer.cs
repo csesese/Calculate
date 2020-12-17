72c90d37c40b58bc57e1f61c513b284d11ef3dca
성은 조 <whtjd999@naver.com>
2020-12-17 17:39:16 +0900

    201217 - 연속 계산 실행중

---
+++ Calculate/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace Calculate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_num_1 = new System.Windows.Forms.Button();
+            this.btn_num_2 = new System.Windows.Forms.Button();
+            this.btn_num_3 = new System.Windows.Forms.Button();
+            this.btn_num_4 = new System.Windows.Forms.Button();
+            this.btn_num_5 = new System.Windows.Forms.Button();
+            this.btn_num_6 = new System.Windows.Forms.Button();
+            this.btn_num_7 = new System.Windows.Forms.Button();
+            this.btn_num_8 = new System.Windows.Forms.Button();
+            this.btn_num_9 = new System.Windows.Forms.Button();
+            this.btn_num_0 = new System.Windows.Forms.Button();
+            this.btn_plumin = new System.Windows.Forms.Button();
+            this.btn_dot = new System.Windows.Forms.Button();
+            this.btn_equal = new System.Windows.Forms.Button();
+            this.btn_plus = new System.Windows.Forms.Button();
+            this.btn_minus = new System.Windows.Forms.Button();
+            this.btn_div = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.btn_c = new System.Windows.Forms.Button();
+            this.btn_mul = new System.Windows.Forms.Button();
+            this.panel1_btn = new System.Windows.Forms.Panel();
+            this.Result = new System.Windows.Forms.Panel();
+            this.text_Result = new System.Windows.Forms.TextBox();
+            this.pre_Result = new System.Windows.Forms.TextBox();
+            this.panel1_btn.SuspendLayout();
+            this.Result.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_num_1
+            // 
+            this.btn_num_1.Location = new System.Drawing.Point(6, 161);
+            this.btn_num_1.Name = "btn_num_1";
+            this.btn_num_1.Size = new System.Drawing.Size(66, 44);
+            this.btn_num_1.TabIndex = 0;
+            this.btn_num_1.Text = "1";
+            this.btn_num_1.UseVisualStyleBackColor = true;
+            this.btn_num_1.Click += new System.EventHandler(this.btn_num_1_Click);
+            // 
+            // btn_num_2
+            // 
+            this.btn_num_2.Location = new System.Drawing.Point(87, 161);
+            this.btn_num_2.Name = "btn_num_2";
+            this.btn_num_2.Size = new System.Drawing.Size(65, 44);
+            this.btn_num_2.TabIndex = 1;
+            this.btn_num_2.Text = "2";
+            this.btn_num_2.UseVisualStyleBackColor = true;
+            this.btn_num_2.Click += new System.EventHandler(this.btn_num_2_Click);
+            // 
+            // btn_num_3
+            // 
+            this.btn_num_3.Location = new System.Drawing.Point(168, 161);
+            this.btn_num_3.Name = "btn_num_3";
+            this.btn_num_3.Size = new System.Drawing.Size(65, 44);
+            this.btn_num_3.TabIndex = 2;
+            this.btn_num_3.Text = "3";
+            this.btn_num_3.UseVisualStyleBackColor = true;
+            this.btn_num_3.Click += new System.EventHandler(this.btn_num_3_Click);
+            // 
+            // btn_num_4
+            // 
+            this.btn_num_4.Location = new System.Drawing.Point(6, 111);
+            this.btn_num_4.Name = "btn_num_4";
+            this.btn_num_4.Size = new System.Drawing.Size(65, 44);
+            this.btn_num_4.TabIndex = 3;
+            this.btn_num_4.Text = "4";
+            this.btn_num_4.UseVisualStyleBackColor = true;
+            this.btn_num_4.Click += new System.EventHandler(this.btn_num_4_Click);
+            // 
+            // btn_num_5
+            // 
+            this.btn_num_5.Location = new System.Drawing.Point(87, 111);
+            this.btn_num_5.Name = "btn_num_5";
+            this.btn_num_5.Size = new System.Drawing.Size(65, 44);
+            this.btn_num_5.TabIndex = 4;
+            this.btn_num_5.Text = "5";
+            this.btn_num_5.UseVisualStyleBackColor = true;
+            this.btn_num_5.Click += new System.EventHandler(this.btn_num_5_Click);
+            // 
+            // btn_num_6
+            // 
+            this.btn_num_6.Location = new System.Drawing.Point(168, 111);
+            this.btn_num_6.Name = "btn_num_6";
+            this.btn_num_6.Size = new System.Drawing.Size(65, 44);
+            this.btn_num_6.TabIndex = 5;
+            this.btn_num_6.Text = "6";
+            this.btn_num_6.UseVisualStyleBackColor = true;
+            this.btn_num_6.Click += new System.EventHandler(this.btn_num_6_Click);
+            // 
+            // btn_num_7
+            // 
+            this.btn_num_7.Location = new System.Drawing.Point(7, 61);
+            this.btn_num_7.Name = "btn_num_7";
+            this.btn_num_7.Size = new System.Drawing.Size(65, 44);
+            this.btn_num_7.TabIndex = 6;
+            this.btn_num_7.Text = "7";
+            this.btn_num_7.UseVisualStyleBackColor = true;
+            this.btn_num_7.Click += new System.EventHandler(this.btn_num_7_Click);
+            // 
+            // btn_num_8
+            // 
+            this.btn_num_8.Location = new System.Drawing.Point(87, 61);
+            this.btn_num_8.Name = "btn_num_8";
+            this.btn_num_8.Size = new System.Drawing.Size(65, 44);
+            this.btn_num_8.TabIndex = 7;
+            this.btn_num_8.Text = "8";
+            this.btn_num_8.UseVisualStyleBackColor = true;
+            this.btn_num_8.Click += new System.EventHandler(this.btn_num_8_Click);
+            // 
+            // btn_num_9
+            // 
+            this.btn_num_9.Location = new System.Drawing.Point(168, 61);
+            this.btn_num_9.Name = "btn_num_9";
+            this.btn_num_9.Size = new System.Drawing.Size(65, 44);
+            this.btn_num_9.TabIndex = 8;
+            this.btn_num_9.Text = "9";
+            this.btn_num_9.UseVisualStyleBackColor = true;
+            this.btn_num_9.Click += new System.EventHandler(this.btn_num_9_Click);
+            // 
+            // btn_num_0
+            // 
+            this.btn_num_0.Location = new System.Drawing.Point(87, 211);
+            this.btn_num_0.Name = "btn_num_0";
+            this.btn_num_0.Size = new System.Drawing.Size(65, 44);
+            this.btn_num_0.TabIndex = 9;
+            this.btn_num_0.Text = "0";
+            this.btn_num_0.UseVisualStyleBackColor = true;
+            this.btn_num_0.Click += new System.EventHandler(this.btn_num_0_Click);
+            // 
+            // btn_plumin
+            // 
+            this.btn_plumin.Location = new System.Drawing.Point(7, 211);
+            this.btn_plumin.Name = "btn_plumin";
+            this.btn_plumin.Size = new System.Drawing.Size(65, 44);
+            this.btn_plumin.TabIndex = 10;
+            this.btn_plumin.Text = "±";
+            this.btn_plumin.UseCompatibleTextRendering = true;
+            this.btn_plumin.UseVisualStyleBackColor = true;
+            this.btn_plumin.Click += new System.EventHandler(this.btn_plumin_Click);
+            // 
+            // btn_dot
+            // 
+            this.btn_dot.Location = new System.Drawing.Point(168, 211);
+            this.btn_dot.Name = "btn_dot";
+            this.btn_dot.Size = new System.Drawing.Size(65, 44);
+            this.btn_dot.TabIndex = 11;
+            this.btn_dot.Text = ".";
+            this.btn_dot.UseCompatibleTextRendering = true;
+            this.btn_dot.UseVisualStyleBackColor = true;
+            this.btn_dot.Click += new System.EventHandler(this.btn_dot_Click);
+            // 
+            // btn_equal
+            // 
+            this.btn_equal.Location = new System.Drawing.Point(250, 211);
+            this.btn_equal.Name = "btn_equal";
+            this.btn_equal.Size = new System.Drawing.Size(65, 44);
+            this.btn_equal.TabIndex = 12;
+            this.btn_equal.Text = "=";
+            this.btn_equal.UseCompatibleTextRendering = true;
+            this.btn_equal.UseVisualStyleBackColor = true;
+            this.btn_equal.Click += new System.EventHandler(this.btn_equal_Click);
+            // 
+            // btn_plus
+            // 
+            this.btn_plus.Location = new System.Drawing.Point(250, 161);
+            this.btn_plus.Name = "btn_plus";
+            this.btn_plus.Size = new System.Drawing.Size(65, 44);
+            this.btn_plus.TabIndex = 13;
+            this.btn_plus.Text = "+";
+            this.btn_plus.UseCompatibleTextRendering = true;
+            this.btn_plus.UseVisualStyleBackColor = true;
+            this.btn_plus.Click += new System.EventHandler(this.btn_plus_Click);
+            // 
+            // btn_minus
+            // 
+            this.btn_minus.Location = new System.Drawing.Point(250, 111);
+            this.btn_minus.Name = "btn_minus";
+            this.btn_minus.Size = new System.Drawing.Size(65, 44);
+            this.btn_minus.TabIndex = 14;
+            this.btn_minus.Text = "-";
+            this.btn_minus.UseCompatibleTextRendering = true;
+            this.btn_minus.UseVisualStyleBackColor = true;
+            this.btn_minus.Click += new System.EventHandler(this.btn_minus_Click);
+            // 
+            // btn_div
+            // 
+            this.btn_div.Location = new System.Drawing.Point(250, 11);
+            this.btn_div.Name = "btn_div";
+            this.btn_div.Size = new System.Drawing.Size(65, 44);
+            this.btn_div.TabIndex = 15;
+            this.btn_div.Text = "/";
+            this.btn_div.UseCompatibleTextRendering = true;
+            this.btn_div.UseVisualStyleBackColor = true;
+            this.btn_div.Click += new System.EventHandler(this.btn_div_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(7, 11);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(65, 44);
+            this.btn_clear.TabIndex = 16;
+            this.btn_clear.Text = "CE";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(168, 11);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(65, 44);
+            this.btn_del.TabIndex = 17;
+            this.btn_del.Text = "Del";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // btn_c
+            // 
+            this.btn_c.Location = new System.Drawing.Point(87, 11);
+            this.btn_c.Name = "btn_c";
+            this.btn_c.Size = new System.Drawing.Size(65, 44);
+            this.btn_c.TabIndex = 18;
+            this.btn_c.Text = "C";
+            this.btn_c.UseVisualStyleBackColor = true;
+            this.btn_c.Click += new System.EventHandler(this.btn_c_Click);
+            // 
+            // btn_mul
+            // 
+            this.btn_mul.Location = new System.Drawing.Point(250, 61);
+            this.btn_mul.Name = "btn_mul";
+            this.btn_mul.Size = new System.Drawing.Size(65, 44);
+            this.btn_mul.TabIndex = 19;
+            this.btn_mul.Text = "x";
+            this.btn_mul.UseCompatibleTextRendering = true;
+            this.btn_mul.UseVisualStyleBackColor = true;
+            this.btn_mul.Click += new System.EventHandler(this.btn_mul_Click);
+            // 
+            // panel1_btn
+            // 
+            this.panel1_btn.Controls.Add(this.btn_mul);
+            this.panel1_btn.Controls.Add(this.btn_c);
+            this.panel1_btn.Controls.Add(this.btn_del);
+            this.panel1_btn.Controls.Add(this.btn_clear);
+            this.panel1_btn.Controls.Add(this.btn_div);
+            this.panel1_btn.Controls.Add(this.btn_minus);
+            this.panel1_btn.Controls.Add(this.btn_plus);
+            this.panel1_btn.Controls.Add(this.btn_equal);
+            this.panel1_btn.Controls.Add(this.btn_dot);
+            this.panel1_btn.Controls.Add(this.btn_plumin);
+            this.panel1_btn.Controls.Add(this.btn_num_0);
+            this.panel1_btn.Controls.Add(this.btn_num_9);
+            this.panel1_btn.Controls.Add(this.btn_num_8);
+            this.panel1_btn.Controls.Add(this.btn_num_7);
+            this.panel1_btn.Controls.Add(this.btn_num_6);
+            this.panel1_btn.Controls.Add(this.btn_num_5);
+            this.panel1_btn.Controls.Add(this.btn_num_4);
+            this.panel1_btn.Controls.Add(this.btn_num_3);
+            this.panel1_btn.Controls.Add(this.btn_num_2);
+            this.panel1_btn.Controls.Add(this.btn_num_1);
+            this.panel1_btn.Location = new System.Drawing.Point(17, 157);
+            this.panel1_btn.Name = "panel1_btn";
+            this.panel1_btn.Size = new System.Drawing.Size(327, 269);
+            this.panel1_btn.TabIndex = 20;
+            // 
+            // Result
+            // 
+            this.Result.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Result.Controls.Add(this.text_Result);
+            this.Result.Controls.Add(this.pre_Result);
+            this.Result.Location = new System.Drawing.Point(17, 38);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(327, 100);
+            this.Result.TabIndex = 21;
+            // 
+            // text_Result
+            // 
+            this.text_Result.Location = new System.Drawing.Point(139, 64);
+            this.text_Result.Name = "text_Result";
+            this.text_Result.Size = new System.Drawing.Size(175, 21);
+            this.text_Result.TabIndex = 1;
+            this.text_Result.Text = "0";
+            // 
+            // pre_Result
+            // 
+            this.pre_Result.Location = new System.Drawing.Point(183, 19);
+            this.pre_Result.Name = "pre_Result";
+            this.pre_Result.Size = new System.Drawing.Size(131, 21);
+            this.pre_Result.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 449);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.panel1_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1_btn.ResumeLayout(false);
+            this.Result.ResumeLayout(false);
+            this.Result.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_num_1;
+        private System.Windows.Forms.Button btn_num_2;
+        private System.Windows.Forms.Button btn_num_3;
+        private System.Windows.Forms.Button btn_num_4;
+        private System.Windows.Forms.Button btn_num_5;
+        private System.Windows.Forms.Button btn_num_6;
+        private System.Windows.Forms.Button btn_num_7;
+        private System.Windows.Forms.Button btn_num_8;
+        private System.Windows.Forms.Button btn_num_9;
+        private System.Windows.Forms.Button btn_num_0;
+        private System.Windows.Forms.Button btn_plumin;
+        private System.Windows.Forms.Button btn_dot;
+        private System.Windows.Forms.Button btn_equal;
+        private System.Windows.Forms.Button btn_plus;
+        private System.Windows.Forms.Button btn_minus;
+        private System.Windows.Forms.Button btn_div;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.Button btn_c;
+        private System.Windows.Forms.Button btn_mul;
+        private System.Windows.Forms.Panel panel1_btn;
+        private System.Windows.Forms.Panel Result;
+        private System.Windows.Forms.TextBox text_Result;
+        private System.Windows.Forms.TextBox pre_Result;
+    }
+}
+
---
+++ Calculate/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculate
+{
+    public partial class Form1 : Form
+    { 
+        string output; //최종 결과값
+        string cal;//사칙 연산 저장
+        Double R;//사칙연산 후 결과 값
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+        //공통 메서드 : 숫자버튼 클릭하여서 texbox 에 출력
+        public void printResult(string x)
+        {
+            //text가 비어있지 않으면 추가
+            if (text_Result.TextLength != 0)
+            {   //초기값 일때는 제외
+                if (text_Result.Text.Equals("0"))
+                    {
+                    text_Result.Text=x;
+                    }
+                else 
+                    { 
+                    text_Result.AppendText(x); 
+                     }
+               
+            }
+            else text_Result.Text = x;
+        }
+
+        //공통메서드 : 사칙연산버튼을 클릭하여서 위에 pre_Result textbox 에 출력
+/*        public void printPreResult()
+        {
+            pre_Result.Text = text_Result.Text;
+            text_Result.Clear();
+
+        }*/
+
+
+        
+         //공통메서드 : 사칙연산버튼을 클릭하여서 위에 pre_Result textbox 에 출력
+        public void printPreResult()
+        {
+            
+            pre_Result.AppendText(text_Result.Text);
+            text_Result.Clear();
+
+            int pre_str = pre_Result.TextLength;
+            char last_pre_Result = pre_Result.Text[pre_str-1]; //마지막 문자 
+
+            //연산자는 연속사용 불가함으로, 다른 연산자를 선택하면 변경
+            if (text_Result.TextLength != 1)
+            {
+                switch (last_pre_Result)
+                {
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                        pre_Result.Text = pre_Result.Text.Substring(0, pre_str - 1); //마지막 연산 지우기
+                        pre_Result.AppendText(text_Result.Text);
+                        
+                        break;
+                    case '=':
+                        //결과 값에 이어서 연산할때
+                        pre_Result.Clear();
+                        pre_Result.Text = text_Result.Text;
+                        break;
+                    default:
+                        pre_Result.AppendText(text_Result.Text);
+                        text_Result.Clear();
+                        break;
+
+
+                }
+                text_Result.Clear();
+            }
+
+            
+            
+            
+            
+
+        }
+        
+        //숫자 버튼 클릭 
+
+
+        private void btn_num_1_Click(object sender, EventArgs e) 
+        {
+            string num = btn_num_1.Text;
+            printResult(num);
+        }
+
+        private void btn_num_2_Click(object sender, EventArgs e)
+        {
+            string num = btn_num_2.Text;
+            printResult(num);
+        }
+
+        private void btn_num_3_Click(object sender, EventArgs e)
+        {
+            string num = btn_num_3.Text;
+            printResult(num);
+        }
+
+        private void btn_num_4_Click(object sender, EventArgs e)
+        {
+            string num = btn_num_4.Text;
+            printResult(num);
+        }
+
+        private void btn_num_5_Click(object sender, EventArgs e)
+        {
+            string num = btn_num_5.Text;
+            printResult(num);
+        }
+
+        private void btn_num_6_Click(object sender, EventArgs e)
+        {
+            string num = btn_num_6.Text;
+            printResult(num);
+        }
+
+        private void btn_num_7_Click(object sender, EventArgs e)
+        {
+            string num = btn_num_7.Text;
+            printResult(num);
+        }
+
+        private void btn_num_8_Click(object sender, EventArgs e)
+        {
+            string num = btn_num_8.Text;
+            printResult(num);
+        }
+
+        private void btn_num_9_Click(object sender, EventArgs e)
+        {
+            string num = btn_num_9.Text;
+            printResult(num);
+        }
+
+        private void btn_num_0_Click(object sender, EventArgs e)
+        {
+            string num = btn_num_0.Text;
+            printResult(num);
+        }
+
+        
+
+
+
+
+        //CLEAR 현재 입력값을 0으로 초기화
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            text_Result.Clear();
+            text_Result.Text = "0";
+        }
+        
+        //문자열중에 마지막 숫자 지우기 
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            int str = text_Result.TextLength; //현재 문자열 길이
+            
+            if(str==1)
+            {  //한자리 수면 0으로 초기화
+                text_Result.Text = "0";
+            }
+            else
+            {
+                text_Result.Text = text_Result.Text.Substring(0, str - 1);
+            }
+            
+
+        }
+        //모든 것을 초기화
+        private void btn_c_Click(object sender, EventArgs e)
+        {
+            text_Result.Clear();
+            pre_Result.Clear();
+            text_Result.Text = "0";
+        }
+        //소수점 처리
+        private void btn_dot_Click(object sender, EventArgs e)
+        {
+            //중복 체크
+            if (text_Result.Text.Contains(".") == false)
+
+            text_Result.Text += ".";
+        }
+
+        //양수 음수 처리 
+        private void btn_plumin_Click(object sender, EventArgs e)
+        {   
+            //문자열 정수로 변환
+            int outNum = Convert.ToInt32(text_Result.Text);
+            
+            outNum = outNum * -1; 
+
+            //정수를 문자열로 변환
+            string intNum = Convert.ToString(outNum);
+
+            text_Result.Text = intNum ;
+
+
+            
+        }
+
+        //사칙연산 처리
+
+        //덧셈
+        private void btn_plus_Click(object sender, EventArgs e)
+        {
+            printPreResult();
+            pre_Result.AppendText("+");
+
+            cal = "plus";
+
+        }
+
+        //뺄셈
+        private void btn_minus_Click(object sender, EventArgs e)
+        {
+            printPreResult();
+            pre_Result.AppendText("-");
+
+            cal = "minus";
+        }
+        //곱셈
+        private void btn_mul_Click(object sender, EventArgs e)
+        {
+            printPreResult();
+            pre_Result.AppendText("*");
+
+            cal = "mul";
+        }
+
+        //나눗셈
+        private void btn_div_Click(object sender, EventArgs e)
+        {
+            printPreResult();
+            pre_Result.AppendText("÷");
+
+            cal = "div";
+        }
+
+        // - 결과값 출력
+        private void btn_equal_Click(object sender, EventArgs e)
+        {
+
+            //사칙연산 빼고 숫자만             
+            int pre_str=pre_Result.TextLength;
+            
+
+            //문자열을 Double로 변환
+            Double R1 = Convert.ToDouble(pre_Result.Text.Substring(0, pre_str - 1));
+            Double R2 = Convert.ToDouble(text_Result.Text);
+
+            //사칙연산하기 
+           
+            switch (cal)
+            {
+                case "plus": R = R1 + R2; break; //더하기
+                case "minus": R = R1 - R2;break;//빼기
+                case "mul": R = R1 * R2;break;//곱셈
+                case "div": R = R1 / R2;break;//나눗셈
+
+            }
+
+
+            //R 값이 정수이면 True, 실수이면 False
+            if ((R % 1) == 1)
+            {
+                int int_R = (int)R;// int 로 형변환
+                output = int_R.ToString();
+            }
+            else //실수 이면 바로 string으로 변환
+            {
+                output = R.ToString();
+            }
+              
+
+
+            pre_Result.AppendText(text_Result.Text);
+            pre_Result.AppendText("=");
+            //결과값 출력하기 
+            text_Result.Text=output;
+
+            
+
+        }
+
+
+    }
+
+    
+
+}
